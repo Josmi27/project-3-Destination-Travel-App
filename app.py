ed59037b27662da659e32efe6b98755bd6fd1b16
Ubuntu <ubuntu@ip-172-31-92-131.ec2.internal>
2020-02-25 20:49:25 +0000

    working db

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 import os
-import flask
+import flask, flask_socketio, flask_sqlalchemy
+import models
 
 app = flask.Flask(__name__)
+socketio= flask_socketio.SocketIO(app)
+
+
 
 @app.route('/')
-def hello():
-    return flask.render_template('index.html')
+def index():
+    addresses = models.Message.query.all()
+    html = ['<li>' + a.address + '</li>' for a in addresses]
+    return '<ul>' + ''.join(html) + '</ul>'
+    
 
-app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=int(os.getenv('PORT', 8080)),
-    debug=True
-)
+@socketio.on('connect')
+def on_connect():
+    print("Someone connected!")
+    socketio.emit('update',{
+        'data': 'Got your connection'
+    })
+
+
+if __name__ == '__main__':
+    
+    socketio.run(
+        app,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=int(os.getenv('PORT', 8080)),
+        debug=True
+    )
---
+++ models.py
@@ -0,0 +1,18 @@
+import flask_sqlalchemy, app
+
+
+
+
+app.app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://mowhi3:Polytomsu2020@localhost/postgres'
+db = flask_sqlalchemy.SQLAlchemy(app.app)
+
+class Message(db.Usps):
+    id = db.Column(db.Integer, primary_key=True)  # key
+    text = db.Column(db.String(250))
+        
+    def __init__(self, t):
+        self.text = t
+        
+        
+    def __repr__(self):
+        return '<Message text: %s>' % self.text 
