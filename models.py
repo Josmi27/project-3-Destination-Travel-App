2f27c7b90f2c1bfe26ef1981569b93ef4bb0590f
Ubuntu <ubuntu@ip-172-31-83-106.ec2.internal>
2020-04-18 23:28:03 +0000

    updated getenv line in models

--- models.py
+++ models.py
@@ -2,8 +2,8 @@ import flask_sqlalchemy, app, os
 
  
 # os.environ['DATABASE_URL'] = 'postgresql://joyelias:welcome@localhost/postgres'
-# app.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://master:password@localhost/postgres'
+app.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://master:password@localhost/postgres'
 db = flask_sqlalchemy.SQLAlchemy(app.app)
 
 class Message(db.Model):
