e6f6aca875c96cf22202ed396b50c40c6861ac0f
Ubuntu <ubuntu@ip-172-31-83-106.ec2.internal>
2020-04-18 18:04:10 +0000

    updated api and chatbot file

--- chatbot.py
+++ chatbot.py
@@ -1,40 +1,122 @@
+# import os
+# import flask
+# import flask_socketio
+# import models, random
+# import app
+# import jokeapi
+
+# scenes = ['Imagine you are on a beautiful island, away from your problems', 'Imagine you are in your bed, sleeping your troubles away', 'Imagine you are in the library reading your favorite book']
+
+
+# rand_scene = random.randint(0, len(scenes)-1)
+
+# class Chatbot():
+#     def __init__(self):
+#         return 
+#     def response(self, message):
+#         bangs, command, args = message.split(' ', 2)
+        
+#         if command == 'help':
+#             response = 'Want me to say more? I respond to: !! about, !! say <something>, !! quotes, !! joke, !! island, !! tips, !! meditation and !! imagine'
+#         elif command == 'tips':
+#             response = 'Want to know how to relax after a stressful day?: take a shower, prepare you favorite meal, do not think about any of your troubles!'
+#         elif command  == 'about':
+#             response = 'Welcome to the Relaxation Chatroom! Here you will be able to escape from the stresses of your life and relaaaaaaax :)' 
+#         elif command == 'say':
+#             response = args
+#         elif command == 'quotes':
+#             chatbot_quotes=[" Difficult roads often lead to beautiful destinations", "I promise you nothing is as chaotic as it seems", "Act the way that you want to feel.", "Tension is who you think you should be. Relaxation is who you are."]
+#             response = chatbot_quotes[random.randint(0,len(chatbot_quotes)-1)]
+#         elif command == 'imagine':
+#             response = scenes[rand_scene]
+#         elif command == 'meditation':
+#             response = "For more help relaxing, I would recommend downlaoding the Calm application on your phone."
+#         elif command == 'joke':
+#             response = jokeapi.rand_joke
+#         elif command == "island":
+#             response = "For extra relaxation, considering visiting an island in the Caribbean, like the Bahamas!"
+#         else:
+#             response = "I'm sorry, I don't understand what you're saying. Try '!!help for commands I understand.'"
+#         return response
+
+
 import os
 import flask
 import flask_socketio
-import models, random
 import app
 import jokeapi
+import requests
+from jokeapi import current_temperature
+from jokeapi import current_timezone
+from jokeapi import music_from_jamaica
+from jokeapi import music_from_pr
+from jokeapi import flight_api_cost
+from jokeapi import currency_conversion
+from jokeapi import outdoor_activity
+from jokeapi import translate
 
-scenes = ['Imagine you are on a beautiful island, away from your problems', 'Imagine you are in your bed, sleeping your troubles away', 'Imagine you are in the library reading your favorite book']
-
-
-rand_scene = random.randint(0, len(scenes)-1)
 
 class Chatbot():
     def __init__(self):
         return 
+
     def response(self, message):
-        bangs, command, args = message.split(' ', 2)
         
-        if command == 'help':
-            response = 'Want me to say more? I respond to: !! about, !! say <something>, !! quotes, !! joke, !! island, !! tips, !! meditation and !! imagine'
-        elif command == 'tips':
-            response = 'Want to know how to relax after a stressful day?: take a shower, prepare you favorite meal, do not think about any of your troubles!'
-        elif command  == 'about':
-            response = 'Welcome to the Relaxation Chatroom! Here you will be able to escape from the stresses of your life and relaaaaaaax :)' 
-        elif command == 'say':
-            response = args
-        elif command == 'quotes':
-            chatbot_quotes=[" Difficult roads often lead to beautiful destinations", "I promise you nothing is as chaotic as it seems", "Act the way that you want to feel.", "Tension is who you think you should be. Relaxation is who you are."]
-            response = chatbot_quotes[random.randint(0,len(chatbot_quotes)-1)]
-        elif command == 'imagine':
-            response = scenes[rand_scene]
-        elif command == 'meditation':
-            response = "For more help relaxing, I would recommend downlaoding the Calm application on your phone."
-        elif command == 'joke':
-            response = jokeapi.rand_joke
-        elif command == "island":
-            response = "For extra relaxation, considering visiting an island in the Caribbean, like the Bahamas!"
-        else:
-            response = "I'm sorry, I don't understand what you're saying. Try '!!help for commands I understand.'"
+        
+        if message == '!! help':
+            response = 'Please select one of the following: !! about, !! Jamaica, !! Puerto Rico, !! Exit, !! Current Weather, !! more help'
+        elif message == '!! more help':
+            response = 'Please select one of the following: !! say something, !! timezone, !! music , !! flights, !! currency, !! activities'
+        elif message == '!! say something':
+            response = "Hello, I'm a travel bot! For more help, try typing !! help"
+        elif message == '!! music':
+            response = "Please select one of the following: !! Jamaican-Music, !! PR-Music, !! translate"
+        elif message == "!! about":
+            response = "The purpose of this chatbot is inform you of random fun facts regarding specific travel locations!"
+        elif message == '!! Jamaica':
+             response = 'Travel Destination Selected: Jamaica. Fact: Dancehall music is the most popular in Jamaica'
+        elif message == '!! Bali':
+             response = 'Travel Destination Selected: Bali. Fact: the ice in Bali is quality controlled by the local government.'
+        elif message == '!! Puerto Rico':
+            response = "Travel Destination Selected: Puerto Rico. Fact: more than 70% of the rum sold in the United States comes from Puerto Rico."
+        elif message == '!! Exit':
+            response = 'Thank you! More locations and facts available soon!'
+        elif message == '!! Current Weather':
+            response = current_temperature()
+        elif message == '!! timezone':
+            response = current_timezone()
+        elif message == '!! Jamaican-Music':
+            response = music_from_jamaica()
+        elif message == '!! PR-Music':
+            response = music_from_pr()
+        elif message == '!! flights':
+            response = flight_api_cost()
+        elif message == '!! currency':
+            response = currency_conversion()
+        elif message == '!! activities':
+            response = outdoor_activity()
+        elif message == '!! translate':
+            response = translate()
+        # elif message == '!! advice for country code: {}'.format(message[28:]):
+        #     country_code =  "{}".format(message[28:])
+        #     travel_warning_search = "https://www.reisewarnung.net/api?country={}".format(country_code)
+        #     response = requests.get(travel_warning_search)
+        #     json_body = response.json()
+        #     danger_rating = json_body["data"]["situation"]["rating"]
+        #     advice = json_body["data"]["lang"]["en"]["advice"]
+        #     response = "Since the travel danger rating is {}/5, {}".format(danger_rating, advice)
+        # elif message == "!! outdoor activities in Jamaica":
+        #     url = "https://trailapi-trailapi.p.rapidapi.com/"
+        #     querystring = {"q-country_cont":"{}".format(message[25:])}
+        #     headers = {
+        #         'x-rapidapi-host': "trailapi-trailapi.p.rapidapi.com",
+        #         'x-rapidapi-key': "2bc8488dedmsh09812c800fb6b89p19fb19jsn6987dc57cdda"
+        #         }
+        #     response = requests.request("GET", url, headers=headers, params=querystring)
+        #     json_body = response.json()
+        #     activity = json_body["places"][0]["name"]
+        #     activities_response = "You should check out the activity called: {}".format(activity)
+        #     return(activities_response)
+            # else:
+            #     chatbot_message = "I'm not sure what you mean. Try '!! help'"
         return response
--- jokeapi.py
+++ jokeapi.py
@@ -1,20 +1,134 @@
+# import requests
+# import json
+
+
+# import requests
+
+# url = "https://joke3.p.rapidapi.com/v1/joke"
+
+# headers = {
+#     'x-rapidapi-host': "joke3.p.rapidapi.com",
+#     'x-rapidapi-key': "e9030cc69cmsh46998fa37a68441p18e133jsne5b9847bff25"
+#     }
+
+# response = requests.request("GET", url, headers=headers)
+
+# json_body = response.json()
+# rand_joke = json.dumps(json_body["content"], indent=2)
+
+
 import requests
+import random
 import json
 
+#WEATHERBIT API
+def current_temperature():
+    response = requests.post('https://api.weatherbit.io/v2.0/current?city=Baltimore,MD&units=I&key=dfa4efd664ce43b4a69c1da82f70e9b8')
+    json_body = response.json()
+    temperature = (json_body["data"][0]["temp"])
+    state = (json_body["data"][0]["state_code"])
+    city = (json_body["data"][0]["city_name"])
+    local_weather_temp = ("The temperature in your current location of  " + city + ", " + state + " is " + str(temperature) + " degrees Fahrenheit.")
+    return(local_weather_temp)
 
-import requests
+def current_timezone():
+    response = requests.post('https://api.weatherbit.io/v2.0/current?city=Baltimore,MD&units=I&key=dfa4efd664ce43b4a69c1da82f70e9b8')
+    json_body = response.json()
+    timezone = (json_body["data"][0]["timezone"])
+    city = (json_body["data"][0]["city_name"])
+    local_timezone = ("Your current city of " + city + " is in the " + timezone + " timezone.")
+    return(local_timezone)
 
-url = "https://joke3.p.rapidapi.com/v1/joke"
+#genius music api
+def music_from_jamaica():
+    genius_search_jamaica = "https://api.genius.com/search?q=Sean%20Paul"
+    my_headers = {"Authorization": "Bearer lEohxFMUgWxUa1e0TdNhdA79QSdKgPdW8qZ9RHGzbHThB-sDc8H-yEgsV9NkPPRi"}
+    playlist = random.randint(0,8)
+    response = requests.get(genius_search_jamaica, headers=my_headers)
+    json_body = response.json()
+    song = json_body["response"]["hits"][playlist]["result"]["full_title"]
+    Jamaica_song_selection = ("Here you can find a song from/featuring a local Jamaican artist. Song:  " + song)
+    return(Jamaica_song_selection)
 
-headers = {
-    'x-rapidapi-host': "joke3.p.rapidapi.com",
-    'x-rapidapi-key': "e9030cc69cmsh46998fa37a68441p18e133jsne5b9847bff25"
-    }
+def music_from_pr():
+    genius_search_pr = "https://api.genius.com/search?q=Bad%20Bunny"
+    my_headers = {"Authorization": "Bearer lEohxFMUgWxUa1e0TdNhdA79QSdKgPdW8qZ9RHGzbHThB-sDc8H-yEgsV9NkPPRi"}
+    playlist = random.randint(0,8)
+    response = requests.get(genius_search_pr, headers=my_headers)
+    json_body = response.json()
+    song = json_body["response"]["hits"][playlist]["result"]["full_title"]
+    pr_song_selection = ("Here you can find a song from/featuring a local Puerto Rican artist. Song:  " + song)
+    return(pr_song_selection)
+    
+#My Memory API - text translator 
+def translate():
+    api_url = "https://api.mymemory.translated.net/get?q=Hello! Welcome to Destination Travel!&langpair=en|es"
+    response = requests.get(api_url)
+    json_body = response.json()
+    original_translation = json_body["responseData"]["translatedText"]
+    api_url2 = "https://api.mymemory.translated.net/get?q=¡Hola! Bienvenido a Destination Travel!&langpair=es|en"
+    response2 = requests.get(api_url2)
+    json_body2 = response2.json()
+    reverse_translation = json_body2["responseData"]["translatedText"]
+    translated_sentence = (reverse_translation + " translated into Spanish is: "+ original_translation)
+    return(translated_sentence)
 
-response = requests.request("GET", url, headers=headers)
+#Flight API, daily limit of 10
+def flight_api_cost(): 
+    url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/BWI-sky/MBJ-sky/2020-07-05"
+    querystring = {"inboundpartialdate":"2019-12-01"}
+    headers = {
+        'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+        'x-rapidapi-key': "8258d9474bmshc76a1b383e354d4p11989bjsn988778d9991f"
+        }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    json_body = response.json()
+    min_price_flight = json.dumps(json_body, indent=2)
+    price = "This is the lowest price for flights to Jamaica for the month of July $", min_price_flight
+    return(price)
 
-json_body = response.json()
-rand_joke = json.dumps(json_body["content"], indent=2)
+#JAMAICA CURRENCY CONVERSION
+
+def currency_conversion():
+    currency_url = "https://currency-converter5.p.rapidapi.com/currency/convert"
+    querystring = {"format":"json","from":"USD","to":"JMD","amount":"1"}
+    headers = {
+        'x-rapidapi-host': "currency-converter5.p.rapidapi.com",
+        'x-rapidapi-key': "8258d9474bmshc76a1b383e354d4p11989bjsn988778d9991f"
+        }
+    response = requests.request("GET", currency_url, headers=headers, params=querystring)
+    json_body = response.json()
+    conversion = json.dumps(json_body["rates"]["JMD"]["rate"], indent=2)
+    jamaica_conversion = "The currency conversion from the US dollar to the Jamaican Dollar is: $", conversion
+    return(jamaica_conversion)
+
+
+def outdoor_activity():
+    url = "https://trailapi-trailapi.p.rapidapi.com/"
+    headers = {'x-rapidapi-host': "trailapi-trailapi.p.rapidapi.com",'x-rapidapi-key': "2bc8488dedmsh09812c800fb6b89p19fb19jsn6987dc57cdda"}
+    querystring = {"location_id":"1","limit":"30","sort":"relevance","offset":"0","lang":"en_US","currency":"USD","units":"km","query":"pattaya"}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    json_body = response.json()
+    activity = json_body["places"][3]["name"]
+    outdoor_response = "In Puerto Rico, you should check out the activity called: " + activity
+    return(outdoor_response)
+
+
+# THIS IS A BLUEPRINT FOR JOSHUA SMITH'S SECOND API
+
+    # def travel_advice():
+    #     url = "https://www.reisewarnung.net/api?country={PR}"
+    #     response = requests.get(url)
+    #     json_body = response.json()
+    #     danger_rating = json_body["data"]["situation"]["rating"]
+    #     advice = json_body["data"]["lang"]["en"]["advice"]
+    #     advice_response = "Since the travel danger rating is {}/5, {}".format(danger_rating, advice)
+    #     return(advice_response)
+
+
+
+
+#
 
 
 
