938031b22cbf7f7204c5e362d8cb1a0764135ee7
Ubuntu <ubuntu@ip-172-31-83-106.ec2.internal>
2020-04-18 18:04:29 +0000

    updated models file for heroku push

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 import flask_sqlalchemy, app, os
 
  
-
+# os.environ['DATABASE_URL'] = 'postgresql://joyelias:welcome@localhost/postgres'
 app.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://joyelias:welcome@localhost/postgres'
 db = flask_sqlalchemy.SQLAlchemy(app.app)
 
 class Message(db.Model):
